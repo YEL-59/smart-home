e2e7d5b206d3c91c9db6595886fca71ce5b708e8
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-03-19 22:42:29 +0600

    (feat):make global component

---
+++ src/Components/Shareable_Components/Secondary_Title_Section/Secondary_Title_Section.jsx
@@ -0,0 +1,13 @@
+const Secondary_Title_Section = ({secondary_title}) => {
+    return (
+      <>
+        <div>
+          <div className="max-w-lg">
+            <h2 className="text-2xl text-blue-400 font-semibold">{secondary_title}</h2>
+          </div>
+        </div>
+      </>
+    );
+  }
+  
+  export default Secondary_Title_Section

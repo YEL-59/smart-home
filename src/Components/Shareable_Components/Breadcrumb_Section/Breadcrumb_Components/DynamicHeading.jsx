28b594090a6556785149a75ab48abd6d2f97f685
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-03-21 23:44:29 +0600

    (feat):there are seperate multiple small component because of use dynamic breadcoum

---
+++ src/Components/Shareable_Components/Breadcrumb_Section/Breadcrumb_Components/DynamicHeading.jsx
@@ -0,0 +1,26 @@
+import { useLocation } from "react-router-dom";
+
+const DynamicHeading = () => {
+  const location = useLocation();
+  const pathname = location.pathname;
+  const pageTitle = getPageTitle(pathname);
+
+  function getPageTitle(pathname) {
+    // Split the URL path by '/' and get the last segment
+    const segments = pathname.split("/");
+    const lastSegment = segments[segments.length - 1];
+
+    // Convert the last segment to title case
+    return lastSegment
+      .replace(/_/g, " ")
+      .replace(/\b\w/g, (c) => c.toUpperCase());
+  }
+
+  return (
+    <h2 className="text-6xl font-bold text-white text-start mt-32 mb-4">
+      {pageTitle}
+    </h2>
+  );
+};
+
+export default DynamicHeading;

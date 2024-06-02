e8522ab125d621afdfe764d9355cec062046a461
Md. Tofayel Islam <tofayelislam19986@gmail.com>
2024-06-02 23:50:05 +0600

    finally git conflict problem solve

--- src/Components/Shareable_Components/Footer/Footer_Components/Single_SocialIcon/Single_SocialIcon.jsx
+++ src/Components/Shareable_Components/Footer/Footer_Components/Single_SocialIcon/Single_SocialIcon.jsx
@@ -12,22 +12,14 @@ const Single_SocialIcon = ({ icon, hoverIconSrc, alt, link }) => {
         onMouseEnter={() => setIsHovered(true)}
         onMouseLeave={() => setIsHovered(false)}
       >
-<<<<<<< HEAD
-        {isHovered ? <img src={hoverIconSrc} alt={alt} /> : icon}
-=======
         {isHovered && hoverIconSrc ? (
           <img src={hoverIconSrc} alt={alt} />
         ) : (
           icon
         )}
->>>>>>> origin/homepage
       </a>
     </>
   );
 };
 
-<<<<<<< HEAD
 export default Single_SocialIcon
-=======
-export default Single_SocialIcon
->>>>>>> origin/homepage
